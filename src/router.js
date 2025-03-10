76026b2fed2433d15bbc5917722799a16c28458c
Dan Do <dxd3vmb@virginia.edu>
2025-03-09 22:44:03 -0400

    Add mangadex api

--- src/router.js
+++ src/router.js
@@ -22,11 +22,52 @@ router.get('/:user_id/bookmarks/getBookmarks', authenticateToken, async (req, re
     }
 });
 
+router.get('/:user_id/searchManga', authenticateToken, async (req, res) => {
+    if (req.params.user_id !== req.user.id) {
+        return res.status(403).json({ error: "Access denied" });
+    }
+
+    const { title } = req.query;  // Get manga name from query
+
+    if (!title) return res.status(400).json({ error: "Manga title is required" });
+
+    try {
+        const url = `https://api.mangadex.org/manga?title=${encodeURIComponent(title)}&includes[]=cover_art`
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data && data.data) {
+            // array of manga results
+            const results = data.data.map((manga) => {
+                const cover = manga.relationships.find((rel) => rel.type === "cover_art");
+                const coverFileName = cover?.attributes?.fileName;
+                const coverUrl = coverFileName
+                    ? `https://uploads.mangadex.org/covers/${manga.id}/${coverFileName}`
+                    : null;
+
+                const mangaUrl = `https://mangadex.org/title/${manga.id}`;
+
+                return {
+                    id: manga.id,
+                    title: manga.attributes.title.en || "No English Title",
+                    description: manga.attributes.description.en || "No English Description Available",
+                    image_url: coverUrl,
+                    url: mangaUrl
+                }
+            });
+
+            res.status(200).json({ status: "Success", data: results });
+        }
+    } catch (error) {
+        res.status(500).json({ status: "Failed", error: error.message });
+    }
+});
+
 router.post('/:user_id/bookmarks/addBookmark', authenticateToken, async (req, res) => {
     if (req.params.user_id !== req.user.id) {
         return res.status(403).json({ error: "Access denied" });
     }
-    
+
     try {
         let result = await addBookmark(req);
         res.status(200).json({ status: "Success", result });
